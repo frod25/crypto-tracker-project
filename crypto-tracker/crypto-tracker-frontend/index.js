591fcb28e96518e6d5cef47e69aba40ae506b265
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-06 20:51:12 -0400

    search bar is now expanding and shrinking with a click

--- crypto-tracker/crypto-tracker-frontend/index.js
+++ crypto-tracker/crypto-tracker-frontend/index.js
@@ -1,9 +1,20 @@
 const search = document.querySelector("#search-btn")
+const searchInput = document.querySelector("#search-input")
 const addNew = document.querySelector("#new-btn")
 const modalCreateNew = document.querySelector("#modal-create-new")
 
 search.addEventListener('click', e => {
   // on click, toggle to something else
+  if (searchInput.style.width == "0px") {
+    searchInput.style.width = "200px"
+    searchInput.style.padding = "0 10px"
+    search.style.borderRadius = "0 5px 5px 0"
+    // also give the searchbtn a border-radius of something
+  } else {
+    searchInput.style.padding = "0px"
+    searchInput.style.width = "0px"
+    search.style.borderRadius = "5px"
+  }
 
 })
 
