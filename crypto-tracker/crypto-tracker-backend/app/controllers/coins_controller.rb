8285e58bf150e9ceed87edc5ba5b9cef5db9e558
frod25 <frod25>
2020-07-07 23:25:01 -0400

    Revert "Completed basic backend CRUD logic and fetch wrapper methods"
    This reverts commit 092916f58275d2d6846ca7d00171fb89358c018a.

--- crypto-tracker/crypto-tracker-backend/app/controllers/coins_controller.rb
+++ crypto-tracker/crypto-tracker-backend/app/controllers/coins_controller.rb
@@ -4,46 +4,4 @@ class CoinsController < ApplicationController
         coins = Coin.all
         render json: coins
     end
-
-    def create
-        coin = Coin.create(
-            name: params[:name],
-            ticker_symbol: params[:tickerSymbol],
-            ticker_image: params[:tickerImage],
-            landing_page: params[:landingPage],
-            status: params[:status],
-            color: params[:color],
-            balance: params[:balance]
-        )
-        if coin.valid?
-            render json: coin
-        else
-            render json: {errors: coin.errors.full_messages}, status: 400
-        end
-    end
-
-    def destroy
-        coin = Coin.find(params[:id])
-        coin.destroy
-        render json: {message: "Coin deleted"}
-    end
-
-    def update
-        coin = Coin.find(params[:id])
-        updated_params = {params.keys.first => params[params.keys.first]}
-        if coin.update(updated_params)
-            render json: coin
-        else
-            render json: {errors: coin.errors.full_messages}, status: 400
-        end
-    end
-
-    def show
-        coin = Coin.find(params[:id])
-        if coin
-            render json: coin
-        else
-            render json: {errors: "Coin not found"}
-        end
-    end
 end
