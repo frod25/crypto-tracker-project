87b8a7e816a9e9bf6a8c09fad99f22ba5d1cbe0f
frod25 <frod25>
2020-07-08 00:07:18 -0400

    attempting to add back all the crud features

---
+++ crypto-tracker/crypto-tracker-frontend/src/fetch.js
@@ -0,0 +1,40 @@
+const baseUrl = "http://localhost:3000/coins/"
+
+function getAllCoins(){
+    return fetch(baseUrl)
+    .then(res => res.json())
+}
+
+function createNewCoin(coinObj){
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(coinObj)
+    })
+    .then(res => res.json())
+}
+
+function getOneCoin(coinId){
+    return fetch(baseUrl + `${coinId}`)
+    .then(res => res.json())
+}
+
+function deleteCoin(coinId){
+    return fetch(baseUrl + `${coinId}`)
+    .then(res => res.json())
+}
+
+function updateCoin(coinId, coinObj){
+    return fetch(baseUrl + `${coinId}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(coinObj)
+    })
+    .then(res => res.json())
+}
